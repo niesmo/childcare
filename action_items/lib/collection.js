5db8493ec54ad316f7b2668980abfa782c6d6b1a
Nima Esmaili Mokaram <esmailimokaram.1@osu.edu>
2015-09-15 19:29:48 -0400

    Added the collections for action items and students

---
+++ action_items/lib/collection.js
@@ -0,0 +1,56 @@
+ActionItems = new Mongo.Collection('action_items');
+
+var Schemas = {};
+
+Schemas.ActionItem = new SimpleSchema({
+    title: {
+        type: String,
+        label: "Title",
+        max: 200
+    },
+
+    description: {
+        type: String,
+        label: "Description",
+        max: 4096
+    },
+
+    /**
+     * Urgency of the Action Item. Can take the values of:
+     * 1. LOW
+     * 2. MEDIUM
+     * 3. HIGH
+     */
+    urgency: {
+        type: String,
+        label: "Urgency",
+        optional: true
+    },
+
+    /**
+     * Priority of the Action Item. Can take the values of:
+     * 1. LOW
+     * 2. MEDIUM
+     * 3. HIGH
+     */
+    priority: {
+        type: String,
+        label: "Priority",
+        optional: true
+    },
+
+    createdBy: {
+        type: SimpleSchema.RegEx.Id
+    },
+
+    completedBy: {
+        type: SimpleSchema.RegEx.Id,
+        optional: true
+    },
+
+    createdAt: {
+        type: Date
+    },
+});
+
+ActionItems.attachSchema(Schemas.ActionItem)
---
+++ students/lib/collection.js
@@ -0,0 +1,63 @@
+Students = new Mongo.Collection('students');
+
+var Schemas = {};
+
+Schemas.Student = new SimpleSchema({
+    firstName: {
+        type: String,
+        label: "First Name",
+        max: 45
+    },
+    middleName: {
+        type: String,
+        label: "Middle Name",
+        max: 45,
+        optional: true
+    },
+    lastName: {
+        type: String,
+        label: "Last Name",
+        max: 45
+    },
+    dateOfBirth: {
+      type: Date,
+      label: "Date of Birth"
+    },
+
+    /**
+     * status would can only be values of
+     * 1. "WAITLIST"
+     * 2. "APPLICATION"
+     * 3. "ENROLLED"
+     */
+    status:{
+      type: String,
+      label: "Status",
+    },
+
+    /**
+     * order will capture the order of the student in the current waitlist
+     */
+    order:{
+      type: Number,
+      label: "Order",
+      optional: true
+    },
+
+    /**
+     * type will only get the values of
+     * 1. REGULAR   ->  First time students
+     * 2. MEMBER    ->  Student of a member of the organization
+     * 3. EXISTING  ->  Already a member student (their parent have another student here)
+     */
+    type:{
+      type: String,
+      label: "Type",
+    },
+
+    createdAt: {
+        type: Date
+    },
+});
+
+Students.attachSchema(Schemas.Student);
