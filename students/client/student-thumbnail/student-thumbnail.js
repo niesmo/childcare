f964f90e9f530945185460402d656203e9deb3fe
Nima Esmaili Mokaram <esmailimokaram.1@osu.edu>
2015-09-29 03:58:14 -0400

    Somewhat got close to finishing the students page

---
+++ students/client/student-thumbnail/student-thumbnail.js
@@ -0,0 +1,9 @@
+Template.studentThumbnail.events({
+  'click img.student-avatar': function (e, tpl) {
+    // find the id of the selected student
+    var id = e.target.id.substr(6);
+
+    // set the session value to that student
+    Session.set('selectedStudentId', id);
+  }
+});
