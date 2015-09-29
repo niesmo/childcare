15adccf45617da617d03bac5750526dc0d008143
Nima Esmaili Mokaram <esmailimokaram.1@osu.edu>
2015-09-29 01:24:31 -0400

    Added a simple template for students to show their avatar

--- students/client/students.js
+++ students/client/students.js
@@ -1,3 +1,23 @@
+/** Subscribe to all the publications when the template is created */
 Template.students.onCreated(function(){
   this.subscribe('enrolledStudents');
-})
+});
+
+Template.students.helpers({
+  /**
+   * This helper sends data to the template about all the infants
+   * @return {Meteor.cursor} The cursor to the db
+   */
+  infants: function () {
+    return Students.find({group: "INFANT"});
+  },
+
+
+  /**
+   * This helper sends data to the template about all the toddlers
+   * @return {Meteor.cursor} The cursor to the db
+   */
+  toddlers: function () {
+    return Students.find({group: "TODDLER"});
+  },
+});
