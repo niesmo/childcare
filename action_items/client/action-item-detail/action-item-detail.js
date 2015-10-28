c9d575c04799856845fb43a83334497f64ebe288
SamuelLMoney <sammassari13@live.com>
2015-10-27 22:28:10 -0400

    Added first/last name to registration, name shows up under created/completed by, System messages work correctly, Temporary System Message button, createdBy now optional

--- action_items/client/action-item-detail/action-item-detail.js
+++ action_items/client/action-item-detail/action-item-detail.js
@@ -8,6 +8,9 @@ Template.actionItemDetail.helpers({
     if(!this._id) return;
 
     var actionItem = ActionItems.findOne({_id: this._id});
+    if(actionItem.isSystemMessage){
+      return "SYSTEM";
+    }
     var createdByUser= Meteor.users.findOne(actionItem.createdBy);
     var name=createdByUser.profile.firstName+" "+completedByUser.profile.lastName;
       return name;
@@ -17,6 +20,9 @@ Template.actionItemDetail.helpers({
     if(!this._id) return ;
 
     var actionItem = ActionItems.findOne({_id: this._id});
+    if(actionItem.isSystemMessage){
+      return "SYSTEM";
+    }
     var completedByUser= Meteor.users.findOne(actionItem.completedBy);
     if(completedByUser){
       var name=completedByUser.profile.firstName+" "+completedByUser.profile.lastName;
--- action_items/client/action-items.js
+++ action_items/client/action-items.js
@@ -27,6 +27,9 @@ Template.actionItems.helpers({
     if(!this._id) return;
 
     var actionItem = ActionItems.findOne({_id: this._id});
+    if(actionItem.isSystemMessage){
+      return "SYSTEM";
+    }
     var createdByUser= Meteor.users.findOne(actionItem.createdBy);
     var name=createdByUser.profile.firstName+" "+completedByUser.profile.lastName;
       return name;
@@ -36,6 +39,9 @@ Template.actionItems.helpers({
     if(!this._id) return ;
 
     var actionItem = ActionItems.findOne({_id: this._id});
+    if(actionItem.isSystemMessage){
+      return "SYSTEM";
+    }
     var completedByUser= Meteor.users.findOne(actionItem.completedBy);
     if(completedByUser){
       var name=completedByUser.profile.firstName+" "+completedByUser.profile.lastName;
