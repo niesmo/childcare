1de3d58e8def3acb0d9c84492eb08db6cb9f85b9
Nima Esmaili Mokaram <esmailimokaram.1@osu.edu>
2015-10-29 10:25:44 -0400

    Fixed small bugs with the application form and action item detail.js

--- action_items/client/action-item-detail/action-item-detail.js
+++
@@ -1,6 +0,0 @@
-/*Template.actionItemDetail.helpers({
-  /**
-   * [getCreatedByUser description]
-   * @param  {[type]} actionItemID [description]
-   * @return {[type]}              [description]
-});
