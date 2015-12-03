f844b1d228efb794b2585b1629e80c2d434824cb
ncausey <ncausey@outlook.com>
2015-12-03 13:24:34 -0500

    Updated logic to add students that are partially enrolled to the transition table

--- classrooms/client/classroom-detail/classroom-detail.js
+++ classrooms/client/classroom-detail/classroom-detail.js
@@ -70,10 +70,10 @@ Template.classroomDetail.helpers({
     var currentClassroom = Classrooms.findOne(this._id);
     var numToShowFromWaitlist = 3;
     if (currentClassroom.type == "INFANT") {
-      return Students.find({$and: [{status: "WAITLIST"}, {group: "INFANT"}, {order: {$lte: numToShowFromWaitlist}}]});
+      return Students.find({$or: [{$and: [{status: "WAITLIST"}, {group: "INFANT"}, {order: {$lte: numToShowFromWaitlist}}]}, {$and: [{status: "PARTIALLY_ENROLLED"}, {group: "INFANT"}]}]});
     }
     else if (currentClassroom.type == "TODDLER") {
-      return Students.find({$or: [{$and: [{group: "INFANT"}, {moveDate: {$lte: moveDateRange}}]}, {$and: [{status: "WAITLIST"}, {group: "TODDLER"}, {order: {$lte: numToShowFromWaitlist}}]}]});
+      return Students.find({$or: [{$and: [{group: "INFANT"}, {moveDate: {$lte: moveDateRange}}]}, {$and: [{status: "WAITLIST"}, {group: "TODDLER"}, {order: {$lte: numToShowFromWaitlist}}]}, {$and: [{status: "PARTIALLY_ENROLLED"}, {group: "TODDLER"}]}]});
     }
     //return Students.find({status: "WAITLIST", group: "INFANT"});
   },
