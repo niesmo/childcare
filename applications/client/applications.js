043dd8d872c67a045a56214221d29f7e456b302b
Nima Esmaili Mokaram <esmailimokaram.1@osu.edu>
2015-12-11 12:12:40 -0500

    Update applications.js

--- applications/client/applications.js
+++ applications/client/applications.js
@@ -40,7 +40,7 @@ function applicationSentCompleted(err, applicationToken){
   // show the link that they can access the link in the UI
   $("#application-link").text("The application can be accessed here ");
   var applicationLink = $("<a />")
-    .attr("href", Router.routes['applicationForm'].path({token:applicationToken}))
+    .attr("href", Router.routes['applicationForm'].url({token:applicationToken}))
     .text("link");
 
   $("#application-link").append(applicationLink);
