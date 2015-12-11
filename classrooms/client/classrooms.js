d9e10bb3af0dcbe9dba8f423a0b644b7b8959fe4
alexbollas <azbollas@yahoo.com>
2015-12-11 12:05:44 -0500

    order students in classroom by transition date

--- classrooms/client/classrooms.js
+++ classrooms/client/classrooms.js
@@ -21,7 +21,7 @@ Template.classrooms.helpers({
    * @return {Meteor.cursor}         The cursor to the DB
    */
   studentInClass: function(classId){
-    return Students.find({classId: classId, status:"ENROLLED"});
+    return Students.find({classId: classId, $or: [{status:"ENROLLED"}, {status:"PARTIALLY_ENROLLED"}]});
   },
 
   /**
@@ -30,7 +30,7 @@ Template.classrooms.helpers({
    */
   studentsInThisClass: function(){
     var id = Session.get("selectedClassroomId");    
-    return Students.find({classId: id, status:"ENROLLED"});
+    return Students.find({classId: id, $or: [{status:"ENROLLED"}, {status:"PARTIALLY_ENROLLED"}]});
   },
 
 
