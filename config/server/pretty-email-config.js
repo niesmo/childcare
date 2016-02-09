200b3dbb11c05ed72c37e8c4ac43c9999b5d1497
Nima Esmaili Mokaram <esmailimokaram.1@osu.edu>
2016-02-08 23:23:07 -0500

    updated email config

--- config/server/pretty-email-config.js
+++ config/server/pretty-email-config.js
@@ -1,11 +1,11 @@
 PrettyEmail.options = {
-  from: 'olb-application@ourladyofbethlehem.org',
+  from: 'Childcare Application <application@ourladyofbethlehem.org>',
   logoUrl: 'http://ourladyofbethlehem.org/wp-content/uploads/2015/01/logo_big_new1.jpg',
   companyName: 'Our Lady of Bethlehem',
   companyUrl: 'http://ourladyofbethlehem.org/',
   companyAddress: '4567 Olentangy River Road, Columbus, OH 43214',
   companyTelephone: '(614)459-8285',
-  companyEmail: 'support@mycompany.com',
+  companyEmail: 'contact@ourladyofbethlehem.org',
   siteName: 'Our Lady of Bethlehem'
 }
 
